d295dc940f2614566895d5929bb16d1e6ed2dc6b
Henry Caron <henryacaron@gmail.com>
2022-03-15 11:14:12 -0400

    got target planet tests working

--- test/DFTargetPlanet.test.ts
+++ test/DFTargetPlanet.test.ts
@@ -12,7 +12,7 @@ import {
   increaseBlockchainBlocks,
 } from './utils/TestUtils';
 import { defaultWorldFixture, targetPlanetFixture, World } from './utils/TestWorld';
-import { ADMIN_PLANET_CLOAKED, SPAWN_PLANET_1, SPAWN_PLANET_2 } from './utils/WorldConstants';
+import { LVL0_PLANET_DEEP_SPACE, SPAWN_PLANET_1, SPAWN_PLANET_2 } from './utils/WorldConstants';
 
 describe('Claim Victory', function () {
   let world: World;
@@ -31,7 +31,7 @@ describe('Claim Victory', function () {
     const level = 0;
     const planetType = 1; // asteroid field
     await world.contract.createPlanet({
-      location: ADMIN_PLANET_CLOAKED.id,
+      location: LVL0_PLANET_DEEP_SPACE.id,
       perlin,
       level,
       planetType,
@@ -51,7 +51,7 @@ describe('Claim Victory', function () {
   describe('invading target planet', function () {
     it('player cannot invade target planet without ownership', async function () {
       await expect(
-        world.user1Core.invadeTargetPlanet(...makeRevealArgs(ADMIN_PLANET_CLOAKED, 10, 20))
+        world.user1Core.invadeTargetPlanet(...makeRevealArgs(LVL0_PLANET_DEEP_SPACE, 10, 20))
       ).to.be.revertedWith('you can only invade planets you own');
     });
     describe('player owns target planet', async function () {
@@ -60,13 +60,13 @@ describe('Claim Victory', function () {
         const shipsSent = 50000;
         const silverSent = 0;
         await world.user1Core.move(
-          ...makeMoveArgs(SPAWN_PLANET_1, ADMIN_PLANET_CLOAKED, dist, shipsSent, silverSent)
+          ...makeMoveArgs(SPAWN_PLANET_1, LVL0_PLANET_DEEP_SPACE, dist, shipsSent, silverSent)
         );
       });
       it('player can invade target planet', async function () {
-        await expect(world.user1Core.invadeTargetPlanet(...makeRevealArgs(ADMIN_PLANET_CLOAKED, 10, 20)))
+        await expect(world.user1Core.invadeTargetPlanet(...makeRevealArgs(LVL0_PLANET_DEEP_SPACE, 10, 20)))
           .to.emit(world.contract, 'TargetPlanetInvaded')
-          .withArgs(world.user1.address, ADMIN_PLANET_CLOAKED.id);
+          .withArgs(world.user1.address, LVL0_PLANET_DEEP_SPACE.id);
       });
     });
   });
@@ -77,12 +77,12 @@ describe('Claim Victory', function () {
       const shipsSent = 50000;
       const silverSent = 0;
       await world.user1Core.move(
-        ...makeMoveArgs(SPAWN_PLANET_1, ADMIN_PLANET_CLOAKED, dist, shipsSent, silverSent)
+        ...makeMoveArgs(SPAWN_PLANET_1, LVL0_PLANET_DEEP_SPACE, dist, shipsSent, silverSent)
       );
-      await world.user1Core.invadeTargetPlanet(...makeRevealArgs(ADMIN_PLANET_CLOAKED, 10, 20));
+      await world.user1Core.invadeTargetPlanet(...makeRevealArgs(LVL0_PLANET_DEEP_SPACE, 10, 20));
     });
     it('needs to hold planet for enough time', async function () {
-      await expect(world.user1Core.claimTargetPlanetVictory(ADMIN_PLANET_CLOAKED.id)).to.be.revertedWith(
+      await expect(world.user1Core.claimTargetPlanetVictory(LVL0_PLANET_DEEP_SPACE.id)).to.be.revertedWith(
         'you have not held the planet long enough to claim victory with it'
       );
     });
@@ -96,19 +96,19 @@ describe('Claim Victory', function () {
         );
       });
       it('must own planet to claim victory', async function () {
-        await expect(world.user2Core.claimTargetPlanetVictory(ADMIN_PLANET_CLOAKED.id)).to.be.revertedWith(
+        await expect(world.user2Core.claimTargetPlanetVictory(LVL0_PLANET_DEEP_SPACE.id)).to.be.revertedWith(
           'you can only claim victory with planets you own'
         );
       });
       it('should emit event', async function () {
        
-        const planet = await world.contract.planetsExtendedInfo2(ADMIN_PLANET_CLOAKED.id);
-        await expect(world.user1Core.claimTargetPlanetVictory(ADMIN_PLANET_CLOAKED.id))
+        const planet = await world.contract.planetsExtendedInfo2(LVL0_PLANET_DEEP_SPACE.id);
+        await expect(world.user1Core.claimTargetPlanetVictory(LVL0_PLANET_DEEP_SPACE.id))
           .to.emit(world.contract, 'Gameover')
           .withArgs(world.user1.address);
       });
       it('sets gameover to true and winner to msg sender', async function () {
-        await world.user1Core.claimTargetPlanetVictory(ADMIN_PLANET_CLOAKED.id);
+        await world.user1Core.claimTargetPlanetVictory(LVL0_PLANET_DEEP_SPACE.id);
         const winner = await world.contract.getWinner();
         const gameover = await world.contract.getGameover();
         expect(winner).to.equal(world.user1.address);
