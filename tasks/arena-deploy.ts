89cad3c4b4f641856b1de3076a2e2f331e4be69b
Henry Caron <henryacaron@gmail.com>
2022-03-28 14:47:42 -0400

    refactored arena deployment to work in testing suite

--- tasks/arena-deploy.ts
+++ tasks/arena-deploy.ts
@@ -1,40 +1,59 @@
-import * as fs from 'fs';
 import { task } from 'hardhat/config';
 import type { HardhatRuntimeEnvironment, Libraries } from 'hardhat/types';
 import { DiamondChanges } from '../utils/diamond';
-import * as path from 'path';
-import * as prettier from 'prettier';
-import { tscompile } from '../utils/tscompile';
-
 
 task('arena:create', 'create a lobby from the command line').setAction(deployArena);
 
 async function deployArena({}, hre: HardhatRuntimeEnvironment): Promise<void> {
   console.log("creating lobby and cutting arena facets")
-
   const isDev = hre.network.name === 'localhost' || hre.network.name === 'hardhat';
+
+  const [deployer] = await hre.ethers.getSigners();
+
+  const requires = hre.ethers.utils.parseEther('4');
+  const balance = await deployer.getBalance();
+
+  // Only when deploying to production, give the deployer wallet money,
+  // in order for it to be able to deploy the contracts
+  if (!isDev && balance.lt(requires)) {
+    throw new Error(
+      `${deployer.address} requires ~$${hre.ethers.utils.formatEther(
+        requires
+      )} but has ${hre.ethers.utils.formatEther(balance)} top up and rerun`
+    );
+  }
+
+  return await deployAndCutArena(
+    {ownerAddress: deployer.address, whitelistEnabled: false, initializers: hre.initializers
+
+  }, hre)
+
+}
+
+export async function deployAndCutArena(
+  {
+    ownerAddress,
+    whitelistEnabled,
+    initializers,
+  }: {
+    ownerAddress: string;
+    whitelistEnabled: boolean;
+    initializers: HardhatRuntimeEnvironment['initializers'];
+  },
+  hre: HardhatRuntimeEnvironment
+) {
+  console.log(hre.network.name)
+  const isDev = hre.network.name === 'localhost' || hre.network.name === 'hardhat';
+  const [deployer] = await hre.ethers.getSigners();
 
   const libraries = await deployLibraries({}, hre);
 
   const diamondInit = await deployArenaDiamondInit({}, libraries, hre);
 
-  // Were only using one account, getSigners()[0], the deployer. Becomes the ProxyAdmin
-  const [deployer] = await hre.ethers.getSigners();
-
-  // TODO: The deployer balance should be checked for production.
-  // Need to investigate how much this actually costs.
-
-  const baseURI = isDev ? 'http://localhost:8081' : 'https://zkga.me';
-
-  const contract = await hre.ethers.getContractAt('DarkForest', hre.contracts.CONTRACT_ADDRESS);
-  // console.log(JSON.stringify(contract));
-
-  const { abi: InitABI } = await hre.artifacts.readArtifact('contracts\\DFArenaInitialize.sol:DFArenaInitialize');
+  const { abi: InitABI } = await hre.artifacts.readArtifact('DFArenaInitialize');
   const initInterface = hre.ethers.Contract.getInterface(InitABI);
 
-  const whitelistEnabled = false;
   const artifactBaseURI = '';
-  const initializers = { ...hre.initializers, DISABLE_ZK_CHECKS: true };
 
   const initAddress = diamondInit.address;
   const initFunctionCall = initInterface.encodeFunctionData('init', [
@@ -58,6 +77,7 @@ async function deployArena({}, hre: HardhatRuntimeEnvironment): Promise<void> {
       // Note: The `diamondCut` is omitted because it is cut upon deployment
       ...changes.getFacetCuts('DFArenaCoreFacet', arenaCoreFacet),
       ...changes.getFacetCuts('DFArenaGetterFacet', arenaGetterFacet),
+
     ];
 
     const shouldUpgrade = await changes.verify();
@@ -100,19 +120,14 @@ async function deployArena({}, hre: HardhatRuntimeEnvironment): Promise<void> {
   
     console.log('Arena created successfully. Godspeed cadet.');
 
-
-    const arena = await hre.ethers.getContractAt("DarkForest", address);
-    await saveDeploy(
-      {
-        coreBlockNumber: arenaReceipt.blockNumber,
-        diamondAddress: diamond.address,
-        initAddress: diamondInit.address,
-      },
-      hre
-    );
+    
     return [diamond, diamondInit, arenaReceipt] as const;
   
   }
+
+  const baseURI = isDev ? 'http://localhost:8081' : 'https://zkga.me';
+
+  const contract = await hre.ethers.getContractAt('DarkForest', hre.contracts.CONTRACT_ADDRESS);
 
   function waitForCreated(): Promise<void> {
     return new Promise(async (resolve) => {
@@ -130,7 +145,6 @@ async function deployArena({}, hre: HardhatRuntimeEnvironment): Promise<void> {
   const result = waitForCreated();
 
   const tx = await contract.createLobby(initAddress, initFunctionCall);
-  console.log("hello4")
 
   const receipt = await tx.wait();
   if (!receipt.status) {
@@ -138,38 +152,7 @@ async function deployArena({}, hre: HardhatRuntimeEnvironment): Promise<void> {
   } else {
     console.log("Lobby created successfully");
   }
-
   await result;
-}
-
-export async function deployArenaCoreFacet(
-  {},
-  { LibGameUtils, LibPlanet }: Libraries,
-  hre: HardhatRuntimeEnvironment
-) {
-  const factory = await hre.ethers.getContractFactory('DFArenaCoreFacet', {
-    libraries: {
-      LibGameUtils,
-      LibPlanet,
-    },
-  });
-  const contract = await factory.deploy();
-  await contract.deployTransaction.wait();
-  console.log(`DFArenaCoreFacet deployed to: ${contract.address}`);
-  return contract;
-}
-
-export async function deployArenaGetterFacet(
-  {},
-  { LibGameUtils, LibPlanet }: Libraries,
-  hre: HardhatRuntimeEnvironment
-) {
-  const factory = await hre.ethers.getContractFactory('DFArenaGetterFacet', {
-  });
-  const contract = await factory.deploy();
-  await contract.deployTransaction.wait();
-  console.log(`DFArenaGetterFacet deployed to: ${contract.address}`);
-  return contract;
 }
 
 async function deployArenaDiamondInit({}, { LibGameUtils }: Libraries, hre: HardhatRuntimeEnvironment) {
@@ -229,91 +212,32 @@ export async function deployLibraries({}, hre: HardhatRuntimeEnvironment) {
   };
 }
 
-async function saveDeploy(
-  args: {
-    coreBlockNumber: number;
-    diamondAddress: string;
-    initAddress: string;
-  },
+export async function deployArenaCoreFacet(
+  {},
+  { LibGameUtils, LibPlanet }: Libraries,
   hre: HardhatRuntimeEnvironment
 ) {
-  const isDev = hre.network.name === 'localhost' || hre.network.name === 'hardhat';
+  const factory = await hre.ethers.getContractFactory('DFArenaCoreFacet', {
+    libraries: {
+      LibGameUtils,
+      LibPlanet,
+    },
+  });
+  const contract = await factory.deploy();
+  await contract.deployTransaction.wait();
+  console.log(`DFArenaCoreFacet deployed to: ${contract.address}`);
+  return contract;
+}
 
-  // Save the addresses of the deployed contracts to the `@darkforest_eth/contracts` package
-  const tsContents = `
-  /**
-   * This package contains deployed contract addresses, ABIs, and Typechain types
-   * for the Dark Forest game.
-   *
-   * ## Installation
-   *
-   * You can install this package using [\`npm\`](https://www.npmjs.com) or
-   * [\`yarn\`](https://classic.yarnpkg.com/lang/en/) by running:
-   *
-   * \`\`\`bash
-   * npm install --save @darkforest_eth/contracts
-   * \`\`\`
-   * \`\`\`bash
-   * yarn add @darkforest_eth/contracts
-   * \`\`\`
-   *
-   * When using this in a plugin, you might want to load it with [skypack](https://www.skypack.dev)
-   *
-   * \`\`\`js
-   * import * as contracts from 'http://cdn.skypack.dev/@darkforest_eth/contracts'
-   * \`\`\`
-   *
-   * ## Typechain
-   *
-   * The Typechain types can be found in the \`typechain\` directory.
-   *
-   * ## ABIs
-   *
-   * The contract ABIs can be found in the \`abis\` directory.
-   *
-   * @packageDocumentation
-   */
-
-  /**
-   * The name of the network where these contracts are deployed.
-   */
-  export const NETWORK = '${hre.network.name}';
-  /**
-   * The id of the network where these contracts are deployed.
-   */
-  export const NETWORK_ID = ${hre.network.config.chainId};
-  /**
-   * The block in which the DarkForest contract was initialized.
-   */
-  export const START_BLOCK = ${isDev ? 0 : args.coreBlockNumber};
-  /**
-   * The address for the DarkForest contract.
-   */
-  export const CONTRACT_ADDRESS = '${args.diamondAddress}';
-  /**
-   * The address for the initalizer contract. Useful for lobbies.
-   */
-  export const INIT_ADDRESS = '${args.initAddress}';
-  `;
-
-  const { jsContents, dtsContents } = tscompile(tsContents);
-
-  const contractsFileTS = path.join(hre.packageDirs['@darkforest_eth/contracts'], 'index.ts');
-  const contractsFileJS = path.join(hre.packageDirs['@darkforest_eth/contracts'], 'index.js');
-  const contractsFileDTS = path.join(hre.packageDirs['@darkforest_eth/contracts'], 'index.d.ts');
-
-  const options = prettier.resolveConfig.sync(contractsFileTS);
-
-  fs.writeFileSync(
-    contractsFileTS,
-    prettier.format(tsContents, { ...options, parser: 'babel-ts' })
-  );
-  fs.writeFileSync(
-    contractsFileJS,
-    prettier.format(jsContents, { ...options, parser: 'babel-ts' })
-  );
-  fs.writeFileSync(
-    contractsFileDTS,
-    prettier.format(dtsContents, { ...options, parser: 'babel-ts' })
-  );
+export async function deployArenaGetterFacet(
+  {},
+  {}: Libraries,
+  hre: HardhatRuntimeEnvironment
+) {
+  const factory = await hre.ethers.getContractFactory('DFArenaGetterFacet', {
+  });
+  const contract = await factory.deploy();
+  await contract.deployTransaction.wait();
+  console.log(`DFArenaGetterFacet deployed to: ${contract.address}`);
+  return contract;
 }
