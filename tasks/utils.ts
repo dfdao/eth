f1b04fb7a43e959ef480df90df4accdc96db1807
cha0sg0d <0xcha0sg0d@gmail.com>
2022-04-08 11:30:54 -0700

    feat(hardhat): add revert reason utility

--- tasks/utils.ts
+++ tasks/utils.ts
@@ -1,4 +1,5 @@
-import { subtask } from 'hardhat/config';
+import { ContractTransaction } from 'ethers';
+import { subtask, types } from 'hardhat/config';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
 subtask('utils:assertChainId', 'Assert proper network is selectaed').setAction(assertChainId);
@@ -12,3 +13,23 @@ async function assertChainId({}, hre: HardhatRuntimeEnvironment) {
     );
   }
 }
+
+subtask('utils:logRevertReason', 'log reason for tx revert')
+.addPositionalParam('txHash', 'hash of reverted tx', undefined, types.string)
+.setAction(logRevertReason);
+
+async function logRevertReason(args: {txHash: string}, hre: HardhatRuntimeEnvironment) {
+    const errTx = await hre.ethers.provider.getTransaction(args.txHash);
+    try {
+      //@ts-expect-error
+      await ethers.provider.call(errTx)
+    } catch (err) {
+      // @ts-expect-error
+      console.log(err.error.message)
+    }
+}
+
+
+
+
+
