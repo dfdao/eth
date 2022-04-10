891b5ff49b1c82cbff99934d4869565f48373da6
Henry | 0xhank <71292860+henryacaron@users.noreply.github.com>
2022-04-10 16:52:42 -0400

    fix: testing bugs (#11)

--- test/utils/WorldConstants.ts
+++ test/utils/WorldConstants.ts
@@ -37,7 +37,7 @@ export const manualSpawnInitializers = settings.parse(decodeInitializers, {
   ...defaultInitializerValues,
   MANUAL_SPAWN: true,
   INIT_PERLIN_MIN : 0,
-  INIT_PERLIN_MAXf: 31,
+  INIT_PERLIN_MAX: 31,
 });
 
 export const targetPlanetInitializers = settings.parse(decodeInitializers, {
@@ -47,7 +47,10 @@ export const targetPlanetInitializers = settings.parse(decodeInitializers, {
 });
 
 export const arenaWorldInitializers = settings.parse(decodeInitializers, {
-  ...manualSpawnInitializers,
+  ...defaultInitializerValues,
+  MANUAL_SPAWN: true,
+  INIT_PERLIN_MIN : 0,
+  INIT_PERLIN_MAX: 31,
   TARGET_PLANETS: true,
   TARGET_PLANET_HOLD_BLOCKS_REQUIRED: 10,
 });
